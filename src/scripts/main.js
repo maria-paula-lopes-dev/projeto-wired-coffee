44078e77f3e141daf7961b4a1cd2e185fef0151c
mariapaula-jpg <mariapaulalopessilva5@gmail.com>
2026-05-11 13:30:32 -0300

    Mudança da lógica header e footer
    - Header e footer "automático" nas páginas HTML

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,13 +1,38 @@
-// Selecionando elementos
-const toggle = document.querySelector('.header__toggle');
-const nav = document.querySelector('.header__nav');
+// -- FUNÇÃO PRA BUSCAR O HEADER --
+async function includeHeader() { // Essa tarefa pode demorar por vir de outro arquivo, então continue rodando o site enquanto espera
 
-// Evento de clique
-toggle.addEventListener('click', () => {
+    // Fetch sai da página atual, vai até onde está o header.html e pega ele
+    // Await é um comando de espera. Só passa pra próxima linha quando o fetch voltar com o arquivo
+    const response = await fetch('/src/layouts-html/header.html');
 
-    // Alterna o estado do menu
-    nav.classList.toggle('is-active');
+    // Quando o fetch trás ele vem 'empacotado'. O .text() abre o pacote e transforma o conteúdo em html pra que o navegador consiga ler
+    const html = await response.text();
 
-    // Alterna o estado do botão (troca ícone)
-    toggle.classList.toggle('is-active');
-});
+    // No HTML tem um 'buraco' pro header. O getElementById encontra esse buraco específico. O innerHTML = html pega o código trazido pelo fetch e 'cola' ele lá dentro
+    document.getElementById('header-placeholder').innerHTML = html;
+
+    // Declara as variáveis. Isso vem depois do header
+    const toggle = document.querySelector('.header__toggle');
+    const nav = document.querySelector('.header__nav');
+
+    if (toggle && nav) {
+        toggle.addEventListener('click', () => {
+            // O toggle funciona como um apagador
+            nav.classList.toggle('is-active');
+            toggle.classList.toggle('is-active');
+        });
+    }
+}
+
+async function includeFooter() {
+    // 1. O robô vai buscar o arquivo do rodapé
+    const response = await fetch('/src/layouts-html/footer.html');
+    const html = await response.text();
+
+    // 2. Ele cola o conteúdo no buraco do footer
+    document.getElementById('footer-placeholder').innerHTML = html;
+}
+
+// Executa as duas funções para montar a página
+includeHeader();
+includeFooter();
